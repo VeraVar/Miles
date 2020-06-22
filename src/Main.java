d6167d5e4ea20f2fb2db8087de7e52b1ea5b19ec
Vera Var <primavera1919@yandex.ru>
2020-06-22 15:23:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketPriceInKopecks = 3570099;
+        int indexMile = 20;
+
+        long resultMiles = ticketPriceInKopecks / indexMile / 100;
+        System.out.println(resultMiles);
+    }
+}
